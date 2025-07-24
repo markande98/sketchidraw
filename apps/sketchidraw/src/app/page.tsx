9934c152178b649aebbff97d19cca9cc7442eab4
markande98 <gauravbpit1998@gmail.com>
2025-07-24 16:37:27 +0530

    feat: add prisma in packages/db

--- apps/sketchidraw/src/app/page.tsx
+++ apps/sketchidraw/src/app/page.tsx
@@ -1,3 +1,6 @@
-export default function Home() {
-  return <div>Dashboard component</div>;
+import { prisma } from "@repo/db";
+
+export default async function Home() {
+  const user = await prisma.user.findFirst();
+  return <div>{user?.name ?? "No user added yet"}</div>;
 }
