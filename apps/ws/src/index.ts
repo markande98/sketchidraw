c16d9bd130c487093b95111f99fe9d4d1de1e5d7
markande98 <gauravbpit1998@gmail.com>
2025-08-26 20:17:40 +0530

    fix(websocket): fix env issue

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,11 +1,15 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import {
   ServerConfig,
   WebsocketServerManager,
 } from "./WebsocketServerManager.js";
 
 const config: ServerConfig = {
-  port: parseInt(process.env.PORT || "8080"),
-  heartbeatInterval: 30000,
+  port: parseInt(process.env.PORT || "3001"),
+  heartbeatInterval: parseInt(process.env.HEARTBEAT_INTERVAL || "20000"),
 };
 
 const server = new WebsocketServerManager(config);
