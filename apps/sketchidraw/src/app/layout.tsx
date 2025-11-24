d25004c014b90c912471e89a58ef0890f04b4e81
markande98 <gauravbpit1998@gmail.com>
2025-11-24 09:07:42 +0530

    feat: add web analytics

--- apps/sketchidraw/src/app/layout.tsx
+++ apps/sketchidraw/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -38,6 +39,7 @@ export default async function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Analytics />
           <Toaster />
           <ThemeProvider
             attribute="class"
