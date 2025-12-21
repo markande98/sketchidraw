e96ac6fb4558c3142650baad9e04f8999166883b
markande98 <gauravbpit1998@gmail.com>
2025-12-21 16:35:53 +0530

    fix: add icon

--- apps/sketchidraw/src/app/layout.tsx
+++ apps/sketchidraw/src/app/layout.tsx
@@ -19,6 +19,11 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  icons: {
+    icon: "/images/sketchidraw.svg",
+    shortcut: "/images/sketchidraw.svg",
+    apple: "/images/sketchidraw.svg",
+  },
   title:
     "Sketchidraw | Collaborative Whiteboard 👉 Secure 👋 Hand-drawn look & feel",
   description: "Collaborative Whiteboard with Secure Hand-drawn look & feel",
