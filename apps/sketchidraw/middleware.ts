0e4f0c9014d031a145a1240fbebd478e9b850fc1
markande98 <gauravbpit1998@gmail.com>
2025-12-21 16:59:36 +0530

    fix: pnpm middleware issue- 2

--- apps/sketchidraw/middleware.ts
+++ apps/sketchidraw/middleware.ts
@@ -1,1 +1,13 @@
-export { auth as middleware } from "./auth";
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+
+export const { auth: middleware } = NextAuth(authConfig);
+
+export default middleware;
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|images|.*\\.svg|.*\\.png|.*\\.jpg|.*\\.jpeg|.*\\.gif|.*\\.webp).*)",
+  ],
+};
